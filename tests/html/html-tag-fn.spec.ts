15c59ce294338df2c256d5c1bbb2d65d71b69cd1
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-11-09 19:11:51 -0300

    test: ensure html tag fn add events and elements from array

--- tests/html/html-tag-fn.spec.ts
+++ tests/html/html-tag-fn.spec.ts
@@ -14,4 +14,34 @@ describe('html tag function', () => {
     expect(docFrag.querySelector('a')?.getAttribute('href')).toBe('#')
     expect(docFrag.querySelector('a')?.textContent).toBe('link')
   })
+
+  it('should add all elements from an array', () => {
+    const docFrag = html`
+      <div>${[
+        html`<p id="el-1">any text</p>`,
+        html`<p id="el-2">any value</p>`
+      ]}</div>
+    `
+
+    expect(docFrag.querySelector('#el-1')?.textContent).toBe('any text')
+    expect(docFrag.querySelector('#el-2')?.textContent).toBe('any value')
+  })
+
+  it('should add the correct event to the elements', () => {
+    const fakeFn = jest.fn()
+    const docFrag = html`
+      <div>
+        <button on-click=${fakeFn}>
+          Click me
+        </button>
+      </div>
+
+      <input on-input=${() => fakeFn()} />
+    `
+
+    docFrag.querySelector('button')?.dispatchEvent(new Event('click'))
+    docFrag.querySelector('input')?.dispatchEvent(new Event('input'))
+
+    expect(fakeFn).toHaveBeenCalledTimes(2)
+  })
 })
