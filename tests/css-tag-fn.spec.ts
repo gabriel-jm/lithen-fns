897e1d86f161d7a6f851314515e7d3f1dfeb8bd0
Gabriel Jose <gabriel.josemelo@hotmail.com>
2021-11-10 16:25:02 -0300

    tests: refactor in the way to test the css tag fn

--- tests/css-tag-fn.spec.ts
+++ tests/css-tag-fn.spec.ts
@@ -2,18 +2,54 @@ import { css } from '@/index'
 
 describe('CSS tag function', () => {
   it('should return a minified css text', () => {
-    const html = css`
-      :host([disabled]) {
-        padding: 10px 10px;
-      }
-      
-      section > p {
-        color: red;
-      }
-    `
+    const testsList = [
+      {
+        input: css`
+          :host([disabled]) {
+            padding: 10px 10px;
+          }
+        `,
+        output: ':host([disabled]){padding:10px 10px;}'
+      },
+      {
+        input: css`
+          section > p {
+            color: red;
+          }
+        `,
+        output: 'section > p{color:red;}'
+      },
+      {
+        input: css`
+          input[name~=na] {
+            text-shadow: 10px 2px rgba(0,0,0,0.5)
+          }
+        `,
+        output: 'input[name~=na]{text-shadow:10px 2px rgba(0,0,0,0.5)}'
+      },
+      {
+        input: css`
+          .btn:disabled, #btn:not(.btn) {
+            color: gray;
+          }
+        `,
+        output: '.btn:disabled, #btn:not(.btn){color:gray;}'
+      },
+      {
+        input: css`
+          ::part(div) {
+            width: 100px;
+            height: 200px;
 
-    expect(html.toString()).toBe(
-      ':host([disabled]){padding:10px 10px;}section > p{color:red;}'
-    )
+            display: block;
+          }
+        `,
+        output: '::part(div){width:100px;height:200px;display:block;}'
+      }
+    ]
+    
+    for (const { input, output } of testsList) {
+      expect(input.toString()).toBe(output)
+    }
   })
 })
